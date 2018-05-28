e08b811bb20cf0ad688e2af18d20cbe9c57d032c
Raghu Betina <raghu@firstdraft.com>
2018-05-28 22:23:01 +0000

    Whitelist columns in Product for ActiveAdmin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Product do
+
+ permit_params :brand, :item, :shade
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
