5b364fcf6b91aecd452bdd9df99850af1985caa0
Raghu Betina <raghu@firstdraft.com>
2018-05-28 22:22:34 +0000

    Direct association between products and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  belongs_to :product
+
   belongs_to :skintone
 
   # Indirect associations
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
